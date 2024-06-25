f8e85e89c485b0393171cf30aaa4f60281bf3f1b
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-06-25 16:13:36 -0700

    update routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/Home";
 
 function App() {
-
   return (
     <>
-      <p>test</p>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
