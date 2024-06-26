5a9d8890445e04b9a82f21ff2154f9091b0db4c0
Jeffuz <jeffzhang0049@gmail.com>
2024-06-25 20:11:39 -0700

    setup backend + database

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+
+import express from 'express'
+import dotenv from 'dotenv'
+
+import connectDB from './config/db.js'
+
+dotenv.config()
+const port = process.env.PORT || 5000;
+
+connectDB()
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, ()=> console.log(`Server running on port: ${port}`));
